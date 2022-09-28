4572c96228db0a3c3e4385573a9f86bcce9c2185
Dan Cross <dancross96@gmail.com>
2022-09-28 14:29:33 -0500

    added photo, hardcoded, into webpage. Commented out other feature for now, but will add again later

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import NavTop from './components/Nav.js'
 import BelowNav from './components/BelowNav.js'
 import BelowNavText from './components/BelowNavText.js'
+import AthleteData from './components/Group.js'
 
 function App() {
   return (
     <div className="App">
       <NavTop />
-      <BelowNav />
-      <BelowNavText />
+      <AthleteData />
+      {/* <BelowNav />
+      <BelowNavText /> */}
     </div>
   );
 }
---
+++ src/components/Group.js
@@ -0,0 +1,8 @@
+import Athlete from './Athlete.png'
+
+
+export default function AthleteData() {
+  return <div className="AthleteData">
+    <img className="AthletePhoto" src={Athlete} alt="Athlete" />
+  </div>
+}
