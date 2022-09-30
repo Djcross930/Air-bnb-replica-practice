65a5a6be79ea9f1592cce0b593aec1a8c27dbf8e
Dan Cross <dancross96@gmail.com>
2022-09-30 00:34:04 -0500

    i messed it all up

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import NavTop from './components/Nav.js'
 import BelowNav from './components/BelowNav.js'
 import BelowNavText from './components/BelowNavText.js'
 import AthleteData from './components/Group.js'
-
 function App() {
   return (
     <div className="App">
       <NavTop />
       <BelowNav />
       <BelowNavText />
-      <AthleteData />
+      <AthleteData
+        img="/static/media/Athlete.1f930914506d68e6a2aa.png"
+        review="5.0"
+        reviewCount={6}
+        description="Life lessons with Katie Zaferes"
+        price={136}
+        country="USA"
+      />
     </div>
   );
 }
--- src/components/Group.js
+++ src/components/Group.js
@@ -1,9 +1,15 @@
-import Athlete from './Athlete.png'
 import BelowImage from './BelowImage.js'
 
-export default function AthleteData() {
+export default function AthleteData(props) {
+  console.log(props)
   return <div className="AthleteData">
-    <img className="AthletePhoto" src={Athlete} alt="Athlete" />
-    <BelowImage />
+    <img className="AthletePhoto" src={props.img} alt="Athlete" />
+    <BelowImage
+      review={props.review}
+      reviewCount={props.reviewCount}
+      description={props.description}
+      price={props.price}
+      country={props.country}
+    />
   </div>
 }
